26ca8c288276bbf3b347aafc8883bd725ca74cf7
Julia Sukhova <suxova1998@gmail.com>
2023-05-23 08:52:36 +0300

    Задача10  решена

---
+++ homework2/Ex1.py
@@ -0,0 +1,13 @@
+#Задача 10
+
+n = int(input("Введите количество монеток: "))
+
+coins = input("Введите расположение монеток что на столе лежат (R - решка вверх, G - герб вверх): ")
+
+counter = 0
+
+if coins:
+    for coin in coins:
+        if coin != coins[0]:
+            counter += 1git
+print( counter)
