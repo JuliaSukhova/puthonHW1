ebc29fec2b77f35735293376a70a61c0d7acbf40
Julia Sukhova <suxova1998@gmail.com>
2023-06-02 11:36:10 +0300

    Задача26

--- homework5/ex1.py
+++ homework5/ex1.py
@@ -3,11 +3,11 @@
 A = int(input())
 B = int(input())
 
-def expanent(A,B):
+def degree(A,B):
     if B == 0:
         return 1
     else:
-        return A * expanent(A, B - 1)
+        return A * degree(A, B - 1)
 
-print(expanent(A,B))
+print(degree(A,B))
 
